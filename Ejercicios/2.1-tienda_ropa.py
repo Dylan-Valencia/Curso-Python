a361db758e208e65528956c6a2935421b87ed601
Dylan-Valencia <99683901+Dylan-Valencia@users.noreply.github.com>
2022-02-14 19:36:54 -0500

    Update 2.1-tienda_ropa.py

--- Ejercicios/2.1-tienda_ropa.py
+++ Ejercicios/2.1-tienda_ropa.py
@@ -1,8 +1,10 @@
 """
 Aplicar la plantilla: "{ropa} cuesta {numero} euros" a dos prendas de ropa
 """
-
-
+plantilla="{ropa}:\n cuesta {valor} euros."
+texto_limpio1=plantilla.format(ropa="pantalon", valor="doce")
+texto_limpio2=plantilla.format(ropa="camiseta", valor="diez")
 """
 Aplicar la plantilla: "Su carro de la compra tiene: <formato_aplicado1> y <formato_aplicado2>"
 """
+plantilla_2="Su carro de la compra tiene {} y {}." .format(plantilla)
